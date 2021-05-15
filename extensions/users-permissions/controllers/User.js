a4cb9c893fe459b531c5928c969f974d832b7c18
realabbas <irizviali@gmail.com>
2021-05-15 15:06:22 +0530

    Add 2FA OTP

---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,121 @@
+const twilio = {
+  id: "AC436947d80991ee7cae6d4a52978f3c61",
+  token: "c339e4f4c8d0c107e2ac7e2bade9f677",
+  phone: "+16178588313",
+};
+
+const smsClient = require("twilio")(twilio.id, twilio.token);
+const { sanitizeEntity } = require('strapi-utils');
+
+const sanitizeUser = user =>
+  sanitizeEntity(user, {
+    model: strapi.query('user', 'users-permissions').model,
+  });
+
+module.exports = {
+  async create(ctx) {
+    const { phone, username } = ctx.request.body;
+
+    if (!phone) return ctx.badRequest("missing.phone");
+    if (!username) return ctx.badRequest("missing.username");
+
+    const userWithThisNumber = await strapi
+      .query("user", "users-permissions")
+      .findOne({ phone });
+
+    if (userWithThisNumber) {
+      return ctx.badRequest(null, [
+        {
+          message: [
+            {
+              id: "Auth.form.error.phone.taken",
+              message: "Phone already taken.",
+              field: ["phone"],
+            },
+          ],
+        },
+      ]);
+    }
+    // return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
+
+    const token = Math.floor(Math.random() * 90000) + 10000;
+
+    const user = {
+      username,
+      phone,
+      provider: "local",
+      token,
+    };
+
+    const advanced = await strapi
+      .store({
+        environment: "",
+        type: "plugin",
+        name: "users-permissions",
+        key: "advanced",
+      })
+      .get();
+
+    const defaultRole = await strapi
+      .query("role", "users-permissions")
+      .findOne({ type: advanced.default_role }, []);
+
+    user.role = defaultRole.id;
+
+    try {
+      const data = await strapi.plugins["users-permissions"].services.user.add(
+        user
+      );
+      await smsClient.messages.create({
+        to: phone,
+        from: twilio.phone,
+        body: `Your verification code is ${token}`,
+      });
+      ctx.created(sanitizeUser(data));
+    } catch (error) {
+      ctx.badRequest(null, [{ messages: [{ error }] }]);
+    }
+  },
+  async verifyAccount(ctx) {
+    const { phone, token } = ctx.request.body;
+
+    if (!phone) return ctx.badRequest("missing.phone");
+    if (!token) return ctx.badRequest("missing.token");
+
+    const verifyUserCode = await strapi
+      .query("user", "users-permissions")
+      .findOne({ phone, token });
+
+    if (!verifyUserCode) {
+      return ctx.badRequest(null, [
+        {
+          messages: [
+            {
+              id: "Auth.form.error.code.invalid",
+              message: "Invalid Code or Number",
+              field: ["phone"],
+            },
+          ],
+        },
+      ]);
+    }
+
+    let updateData = {
+      token: "",
+      confirmed: true,
+    };
+    // console.log("ID",{id})
+
+    const { user: userService, jwt: jwtService } = strapi.plugins['users-permissions'].services;
+    const user = await userService.fetch({ phone }, []);
+
+    const data = await strapi.plugins["users-permissions"].services.user.edit(
+    user.id ,
+      updateData
+    );
+    const jwt = strapi.plugins["users-permissions"].services.jwt.issue({
+      id: data.id,
+    });
+    ctx.send({ jwt, user: sanitizeUser(data) });
+  },
+};
