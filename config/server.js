977cc3ab74fd8f65fed23dbe86612d7130dbb833
realabbas <irizviali@gmail.com>
2021-05-15 12:51:45 +0530

    Add Few Collection Types and JWT Auth

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0352cd88cc89952e53317ad4f132649'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd6f35f98-b997-4ecc-a83b-fba7a5eb3f83'
+};
