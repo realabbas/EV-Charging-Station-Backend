fd5764a8168ae3341590d690539dd83972c08727
realabbas <irizviali@gmail.com>
2021-05-17 03:41:32 +0530

    Add Error Handling in Verify API

--- extensions/users-permissions/controllers/User.js
+++ extensions/users-permissions/controllers/User.js
@@ -11,8 +11,6 @@ const sanitizeUser = (user) =>
   sanitizeEntity(user, {
     model: strapi.query("user", "users-permissions").model,
   });
-const emailRegExp =
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 module.exports = {
   async create(ctx) {
@@ -186,8 +184,8 @@ module.exports = {
     //   updateData
     // );
     await strapi
-    .query("user", "users-permissions")
-    .update({ phone }, updateData);
+      .query("user", "users-permissions")
+      .update({ phone }, updateData);
 
     console.log("DATA", user_data);
 
@@ -200,165 +198,6 @@ module.exports = {
     // ctx.send({ jwt, user: sanitizeUser(data),  });
     ctx.send({ jwt, user: user_data });
   },
-  async checks(ctx) {
-    ctx.send({ check: true });
-  },
-  // async login(ctx) {
-  //   const provider = ctx.params.provider || 'local';
-  //   const params = ctx.request.body;
-
-  //   const store = await strapi.store({
-  //     environment: '',
-  //     type: 'plugin',
-  //     name: 'users-permissions',
-  //   });
-
-  //   if (provider === 'local') {
-  //     if (!_.get(await store.get({ key: 'grant' }), 'email.enabled')) {
-  //       return ctx.badRequest(null, 'This provider is disabled.');
-  //     }
-
-  //     // The identifier is required.
-  //     if (!params.identifier) {
-  //       return ctx.badRequest(
-  //         null,
-  //         formatError({
-  //           id: 'Auth.form.error.email.provide',
-  //           message: 'Please provide your username or your e-mail.',
-  //         })
-  //       );
-  //     }
-
-  //     // The password is required.
-  //     if (!params.password) {
-  //       return ctx.badRequest(
-  //         null,
-  //         formatError({
-  //           id: 'Auth.form.error.password.provide',
-  //           message: 'Please provide your password.',
-  //         })
-  //       );
-  //     }
-
-  //     const query = { provider };
-
-  //     // Check if the provided identifier is an email or not.
-  //     const isEmail = emailRegExp.test(params.identifier);
-
-  //     // Set the identifier to the appropriate query field.
-  //     if (isEmail) {
-  //       query.email = params.identifier.toLowerCase();
-  //     } else {
-  //       query.username = params.identifier;
-  //     }
-
-  //     // Check if the user exists.
-  //     const user = await strapi.query('user', 'users-permissions').findOne(query);
-
-  //     if (!user) {
-  //       return ctx.badRequest(
-  //         null,
-  //         formatError({
-  //           id: 'Auth.form.error.invalid',
-  //           message: 'Identifier or password invalid.',
-  //         })
-  //       );
-  //     }
-
-  //     if (
-  //       _.get(await store.get({ key: 'advanced' }), 'email_confirmation') &&
-  //       user.confirmed !== true
-  //     ) {
-  //       return ctx.badRequest(
-  //         null,
-  //         formatError({
-  //           id: 'Auth.form.error.confirmed',
-  //           message: 'Your account email is not confirmed',
-  //         })
-  //       );
-  //     }
-
-  //     if (user.blocked === true) {
-  //       return ctx.badRequest(
-  //         null,
-  //         formatError({
-  //           id: 'Auth.form.error.blocked',
-  //           message: 'Your account has been blocked by an administrator',
-  //         })
-  //       );
-  //     }
-
-  //     // The user never authenticated with the `local` provider.
-  //     if (!user.password) {
-  //       return ctx.badRequest(
-  //         null,
-  //         formatError({
-  //           id: 'Auth.form.error.password.local',
-  //           message:
-  //             'This user never set a local password, please login with the provider used during account creation.',
-  //         })
-  //       );
-  //     }
-
-  //     const validPassword = await strapi.plugins[
-  //       'users-permissions'
-  //     ].services.user.validatePassword(params.password, user.password);
-
-  //     if (!validPassword) {
-  //       return ctx.badRequest(
-  //         null,
-  //         formatError({
-  //           id: 'Auth.form.error.invalid',
-  //           message: 'Identifier or password invalid.',
-  //         })
-  //       );
-  //     } else {
-  //       ctx.send({
-  //         jwt: strapi.plugins['users-permissions'].services.jwt.issue({
-  //           id: user.id,
-  //         }),
-  //         user: sanitizeEntity(user.toJSON ? user.toJSON() : user, {
-  //           model: strapi.query('user', 'users-permissions').model,
-  //         }),
-  //         x: user.id
-  //       });
-  //     }
-  //   } else {
-  //     if (!_.get(await store.get({ key: 'grant' }), [provider, 'enabled'])) {
-  //       return ctx.badRequest(
-  //         null,
-  //         formatError({
-  //           id: 'provider.disabled',
-  //           message: 'This provider is disabled.',
-  //         })
-  //       );
-  //     }
-
-  //     // Connect the user with the third-party provider.
-  //     let user, error;
-  //     try {
-  //       [user, error] = await strapi.plugins['users-permissions'].services.providers.connect(
-  //         provider,
-  //         ctx.query
-  //       );
-  //     } catch ([user, error]) {
-  //       return ctx.badRequest(null, error === 'array' ? error[0] : error);
-  //     }
-
-  //     if (!user) {
-  //       return ctx.badRequest(null, error === 'array' ? error[0] : error);
-  //     }
-  //     console.log("USER.ID", user.id)
-  //     ctx.send({
-  //       jwt: strapi.plugins['users-permissions'].services.jwt.issue({
-  //         id: user.id,
-  //       }),
-  //       user: sanitizeEntity(user.toJSON ? user.toJSON() : user, {
-  //         model: strapi.query('user', 'users-permissions').model,
-  //       }),
-  //     });
-  //   }
-  // },
   async profile(ctx) {
     console.log("PROFILE Called", ctx.state);
 
@@ -371,5 +210,8 @@ module.exports = {
 
     ctx.body = sanitizeUser(user);
     ctx.send({ user: sanitizeUser(user) });
+  },
+  async status(ctx) {
+    ctx.send({ status: 200 });
   },
 };
