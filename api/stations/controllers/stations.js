6d7e7b40fa14e2e7228981c19201d50e89c0f8b9
realabbas <irizviali@gmail.com>
2021-05-20 10:59:40 +0530

    Add Nearby Station API

--- api/stations/controllers/stations.js
+++ api/stations/controllers/stations.js
@@ -1,8 +1,70 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const _ = require("lodash");
+const { sanitizeEntity } = require("strapi-utils");
+
+const sanitizeUser = (user) =>
+  sanitizeEntity(user, {
+    model: strapi.query("user", "users-permissions").model,
+  });
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+  if (lat1 == lat2 && lon1 == lon2) {
+    return 0;
+  } else {
+    var radlat1 = (Math.PI * lat1) / 180;
+    var radlat2 = (Math.PI * lat2) / 180;
+    var theta = lon1 - lon2;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    if (unit == "KM") {
+      dist = dist * 1.609344;
+    }
+    if (unit == "MI") {
+      dist = dist * 0.8684;
+    }
+    return dist;
+  }
+}
+
+module.exports = {
+  async nearbyStation(ctx) {
+    const { latitude, longitude, pincode, radius, unit } = ctx.request.body;
+
+    // query by area first then add the calculate distance function
+
+    const station = await strapi.query("stations").find({ pincode });
+
+    // Empty array for result
+
+    let data;
+
+    // Calculate function
+
+    data = station.filter((index, key) => {
+      let geo = distance(
+        index.latitude,
+        index.longitude,
+        latitude,
+        longitude,
+        unit
+      );
+      return geo > radius ? (index.distance = geo) : null;
+    });
+
+    ctx.send({ resultSize: data.length, data });
+  },
+};
