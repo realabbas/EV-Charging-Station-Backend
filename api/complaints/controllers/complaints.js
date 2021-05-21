a4fb43082755f14da835f9535f9a9ec5eab7a0c2
realabbas <irizviali@gmail.com>
2021-05-22 04:36:47 +0530

    Add JWT-Credentials API for User, Update Routes.json

--- api/complaints/controllers/complaints.js
+++ api/complaints/controllers/complaints.js
@@ -1,8 +1,19 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async userComplaints(ctx) {
+
+    const { id } = ctx.state.user;
+
+    // console.log("ID", id, typeof id)
+
+    const data = await strapi.query("complaints").find({ user_id: id });
+
+    ctx.send({ data, size: data.length });
+  },
+};
