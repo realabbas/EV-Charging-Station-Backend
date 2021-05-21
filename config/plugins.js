85c66be78febc4c999717600a2699bd33f516bec
realabbas <irizviali@gmail.com>
2021-05-22 02:59:42 +0530

    Add AWS S3 Config for Storage

---
+++ config/plugins.js
@@ -0,0 +1,25 @@
+module.exports = ({ env }) => ({
+    upload: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: "AKIA5RJ23HROBSXHBKPE",
+        secretAccessKey: "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",
+        region: "ap-south-1",
+        params: {
+          Bucket: "eden-ev",
+        },
+      },
+    },
+    email: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: "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",
+      },
+      settings: {
+        defaultFrom: "bot@devlab.works",
+        defaultReplyTo: "admin@devlab.works",
+        testAddress: "irizviali@gmail.com",
+      },
+    },
+  });
+  
