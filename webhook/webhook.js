437a143a49f8d6d822a83aa0851abb55f26b6664
realabbas <irizviali@gmail.com>
2021-05-26 12:46:27 +0530

    Add Webhook Service

---
+++ webhook/webhook.js
@@ -0,0 +1,34 @@
+var secret = 'EdenEV';
+var repo = '/home/ubuntu/eden-smart-homes-ev-backend';
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+const PM2_CMD = 'cd ~ && pm2 startOrRestart ecosystem.config.js';
+
+http
+  .createServer(function(req, res) {
+    req.on('data', function(chunk) {
+      let sig =
+        'sha1=' +
+        crypto
+          .createHmac('sha1', secret)
+          .update(chunk.toString())
+          .digest('hex');
+
+      if (req.headers['x-hub-signature'] == sig) {
+        exec(`cd ${repo} && git pull && ${PM2_CMD}`, (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+        });
+      }
+    });
+
+    res.end();
+  })
+  .listen(8080);
