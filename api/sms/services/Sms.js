a988772255fb61e53b102dfdc270a733f8a5eb45
Ubuntu <ubuntu@ip-172-31-8-222.ap-south-1.compute.internal>
2021-05-27 12:30:17 +0000

    Add new SMS API Service provider

--- api/sms/services/Sms.js
+++ api/sms/services/Sms.js
@@ -5,14 +5,36 @@ const twilio = {
 };
 const smsClient = require("twilio")(twilio.id, twilio.token);
 
+const axios = require('axios');
+const config =(to, token)=> {
+    return { 
+      method: 'get',
+      url: `http://hindit.biz/api/pushsms?entityid=1201160672190426707&templateid=1207161795080341079&user=Divyansh&authkey=92bmqjEf8v62&sender=EDENSH&mobile=${to}&text=Your%20one%20time%20password%20for%20eden%20smart%20homes%20verification%20is%20${token}&rpt=1%0A`,
+      headers: { 
+        'x-api-key': '39Ry7Vgo629os6vssExN1qRhAiBx7ms3mLWrIDO7'
+      }
+    }
+};
+    
+
 module.exports = {
-  send: (to, template) => {
+  sendFromTwilio: (to, template) => {
     return smsClient.messages.create({
       to,
       from: twilio.phone,
       body: template,
     });
   },
+  send:(to, token, template)=>{
+    return axios(config(to, token))
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    
+  }
 };
 
 // Usage
