31d728fbb6be336abe222e2e8d47e7e2e25df73c
Ubuntu <ubuntu@ip-172-31-8-222.ap-south-1.compute.internal>
2021-06-05 04:15:19 +0000

    Add Create method in complaints controller

--- api/complaints/controllers/complaints.js
+++ api/complaints/controllers/complaints.js
@@ -4,6 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
   async userComplaints(ctx) {
@@ -15,5 +16,13 @@ module.exports = {
     const data = await strapi.query("complaints").find({ user_id: id });
 
     ctx.send({ data, size: data.length });
+  },
+  async create(ctx) {
+
+    const body = ctx.request.body
+    body.user_id = ctx.state.user.id
+    const entity = await strapi.services.complaints.create(body);
+
+    return sanitizeEntity(entity, { model: strapi.models.complaints });
   },
 };
