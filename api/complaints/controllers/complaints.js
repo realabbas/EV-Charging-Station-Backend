eb1c87df70cc6c95334f527a9861e8c1c64454f3
Ubuntu <ubuntu@ip-172-31-8-222.ap-south-1.compute.internal>
2021-06-11 08:10:52 +0000

    Add Complaints API Constraint

--- api/complaints/controllers/complaints.js
+++ api/complaints/controllers/complaints.js
@@ -21,8 +21,25 @@ module.exports = {
 
     const body = ctx.request.body
     body.user_id = ctx.state.user.id
-    const entity = await strapi.services.complaints.create(body);
 
-    return sanitizeEntity(entity, { model: strapi.models.complaints });
+    const data = await strapi.query("complaints").find({ user_id: body.user_id });
+
+    let active = 0;
+    data.map((i, j) => {
+      i.status === "UNRESOLVED" ? active += 1 : null
+    })
+    console.log("Active beneath map function", active)
+    if(active<=1){
+      console.log("Active inside first condition", active)
+      const entity = await strapi.services.complaints.create(body);
+      return sanitizeEntity(entity, { model: strapi.models.complaints });
+    }
+    else{
+      console.log("Active inside second condition", active)
+
+      ctx.send({ message: "You have 2 active complaints" })
+    }
+    console.log("Active at end", active)
+
   },
 };
